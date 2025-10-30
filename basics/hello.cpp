89086d7b90c63e8c822a6b798264db0049e267cb
navshift404 <navaneethajose004@gmail.com>
2025-10-30 13:56:42 +0530

    hello,input_out

---
+++ basics/hello.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //print hello world
+    cout<< "hello world"<<endl;
+    return 0;
+}
---
+++ basics/input_output.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter the value of x" <<"\n";
+    //to read a vaule from user
+    cin>>x;
+    cout<<"the value of x is " << x << "\n";
+    return 0;
+}
