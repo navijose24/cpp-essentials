238a0a9a46d4d73cabfb4fe5b434dfa3393a59bf
navijose24 <navaneethapjose@gmail.com>
2025-12-02 19:55:51 +0530

    reverse_a_no.

---
+++ basic_maths/reverse_a_no.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"number:";
+    cin>>n;
+    cout<<"reversed no:";
+    while(n!=0){
+        int x=n%10;
+        if(n%10!=0)
+        {    
+            cout<<x;
+            n=n/10;
+        }
+        else n=n/10;
+    }
+
+}
